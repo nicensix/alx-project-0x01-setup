f71149dcc029f27caec732f18dabcee4acb9bd28
Emmanuel Godwin <nicensix@gmail.com>
2025-06-09 09:56:54 +0300

    Task 6 completed, Hurray!

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,33 +1,51 @@
 import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
 import Header from "@/components/layout/Header";
-import { UserProps } from "@/interfaces";
+import { UserData, UserProps } from "@/interfaces";
+import { useState } from "react";
 
-const UsersPage: React.FC<{ posts: UserProps[] }> = ({ posts }) => { 
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [user, setUser] = useState<UserData | null>(null);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUser({ ...newUser, id: posts.length + 1 });
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <h1 className="text-3xl font-bold mb-4">Users</h1>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">User List</h1>
+          <button onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add User
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
           {posts.map((user) => (
             <UserCard key={user.id} {...user} />
           ))}
         </div>
       </main>
+
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
     </div>
   );
 };
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json(); // Keep the variable name as "posts" for checker validation
+  const posts = await response.json();
 
   return {
     props: {
-      posts, // Must match what the checker expects
-    },
+      posts
+    }
   };
 }
 
-const Users = UsersPage;
 export default Users;
